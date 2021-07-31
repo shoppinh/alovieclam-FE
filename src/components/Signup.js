26ff2bd1b382eb4e5fe5b5e5e805ad169f90752b
shoppinh <mactrungkien2000@gmail.com>
2021-07-31 12:09:29 +0700

    xong phan dang nhap va dang ky

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,6 +13,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import MenuItem from "@material-ui/core/MenuItem";
 
 function Copyright() {
   return (
@@ -45,73 +46,317 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  row: {
+    display: "flex",
+    alignItems: "center",
+  },
 }));
+const currencies = [
+  {
+    value: "USD",
+    label: "$",
+  },
+  {
+    value: "EUR",
+    label: "€",
+  },
+  {
+    value: "BTC",
+    label: "฿",
+  },
+  {
+    value: "JPY",
+    label: "¥",
+  },
+];
 
 export default function Signup() {
   const classes = useStyles();
+  const [currency, setCurrency] = React.useState("");
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="md">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign up
+          Đăng ký
         </Typography>
         <form className={classes.form} noValidate>
+          <Typography
+            component="h2"
+            variant="h5"
+            style={{
+              paddingBottom: 5,
+              marginBottom: 5,
+            }}
+          >
+            Thông tin đăng nhập
+          </Typography>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Email đăng nhập :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Địa chỉ email"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Mật khẩu :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Mật khẩu"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Nhập lại mật khẩu :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="retypePassword"
+                  label="Nhập lại mật khẩu"
+                  type="password"
+                  id="retypePassword"
+                  autoComplete="current-password"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
+          </Grid>
+          <Typography
+            component="h2"
+            variant="h5"
+            style={{
+              margin: "20px 0 20px 0",
+            }}
+          >
+            Thông tin nhà tuyển dụng
+          </Typography>
+
+          <Grid container spacing={2}>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Họ và tên :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="companyName"
+                  label="Họ và tên"
+                  id="companyName"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Số điện thoại :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="number"
+                  label="Số điện thoại"
+                  type="password"
+                  id="number"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Giới tính :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="gender"
+                  label="Giới tính"
+                  type="password"
+                  id="gender"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Công ty :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="company"
+                  label="Công ty"
+                  type="password"
+                  id="company"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Địa điểm làm việc :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="location"
+                  label="Địa điểm làm việc"
+                  type="password"
+                  id="location"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Lĩnh vực hoạt động :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  id="jobField"
+                  select
+                  fullWidth
+                  label="Lĩnh vực hoạt động"
+                  onChange={handleChange}
+                  value={currency}
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Quy mô nhân sự :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  id="workforceRange"
+                  select
+                  fullWidth
+                  label="Quy mô nhân sự"
+                  onChange={handleChange}
+                  value={currency}
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Vị trí công tác :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  id="workplace"
+                  select
+                  fullWidth
+                  label="Vị trí công tác"
+                  onChange={handleChange}
+                  value={currency}
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+            <Grid item container className={classes.row}>
+              <Grid item md={3}>
+                <Typography component="h2" variant="subtitle1">
+                  Vị trí tuyển dụng quan tâm :
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="jobPosition"
+                  label="Vị trí tuyển dụng quan tâm"
+                  type="password"
+                  id="jobPosition"
+                  autoComplete="current-password"
+                />
+              </Grid>
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+                label="Tôi đồng ý với điều khoản dịch vụ"
               />
             </Grid>
           </Grid>
@@ -126,9 +371,7 @@ export default function Signup() {
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Đã có tài khoản? Đăng nhập ngay
-              </Link>
+              <Link to="/dang-nhap">Đã có tài khoản? Đăng nhập ngay</Link>
             </Grid>
           </Grid>
         </form>
